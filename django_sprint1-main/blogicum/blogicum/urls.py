f675f68571abb53a26a18e1f8edacf31e08b51fd
whyourelated <whyourelated@gmail.com>
2024-12-18 17:15:23 +0300

    изменила

--- django_sprint1-main/blogicum/blogicum/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('blog.urls', namespace='blog')),
-    path('about/', include('pages.urls', namespace='pages')),
-    path('pages/', include('pages.urls', namespace='pages')),
-    path('admin/', admin.site.urls),
-]
